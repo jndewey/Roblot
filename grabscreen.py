51c10a2aba0877389e9216e403d65c17eefaa431
jndewey <jndewey@gmail.com>
2018-11-24 15:12:42 -0500

    mac test

--- grabscreen.py
+++ grabscreen.py
@@ -34,5 +34,4 @@ def grab_screen(region=None):
     memdc.DeleteDC()
     win32gui.ReleaseDC(hwin, hwindc)
     win32gui.DeleteObject(bmp.GetHandle())
-
     return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
